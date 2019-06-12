61862e3b54ec64a0503c565458dd0df831bc61c1
ljf <2245849546>
2019-06-12 16:43:02 +0800

    实现收发信息逻辑

--- run.py
+++ run.py
@@ -4,6 +4,9 @@ from tornado.web import RequestHandler, Application
 from tornado.ioloop import IOLoop
 from tornado.options import define, options, parse_command_line
 
+import receive
+import reply
+
 define("host", default="8888", help="端口")
 
 
@@ -27,12 +30,27 @@ class WxHandler(RequestHandler):
             hashcode = hashlib.sha1(tmp.encode('utf8')).hexdigest()
 
             if hashcode == signature:
-                print('success')
+                print('微信公众号接入成功')
                 self.write(echostr)
             else:
                 self.write('')
         except Exception as err:
             self.write(err)
+
+    def post(self):
+        try:
+            webData = self.request
+            recMsg = receive.parse_xml(webData)
+            if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
+                toUser = recMsg.FromUserName
+                fromUser = recMsg.ToUserName
+                content = "test"
+                replyMsg = reply.TextMsg(toUser, fromUser, content)
+                self.write(replyMsg.send())
+            else:
+                self.write('success')
+        except Exception as err:
+            return err
 
 
 def make_app():
