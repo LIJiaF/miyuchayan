2a7b2c65e98fe1a3c018d42b5a8530ddfd9d2a93
ljf <1062134582@qq.com>
2019-06-29 22:02:37 +0800

    更改wxConfig配置

--- wxConfig.py
+++ wxConfig.py
@@ -2,8 +2,8 @@ DISCOUNT_ID = "http://wx.winslen.cn/discount"
 PERSONAL_ID = "http://wx.winslen.cn/personal"
 
 # test
-# APPID = "wx4ad79b44d68db8da"
-# APPSECRET = "75b8b1f237c468b41124033ba7a05c4a"
+APPID = "wx4ad79b44d68db8da"
+APPSECRET = "75b8b1f237c468b41124033ba7a05c4a"
 
 # alpha
 # APPID = "wx840d7b62f93a0dd9"
@@ -14,5 +14,5 @@ PERSONAL_ID = "http://wx.winslen.cn/personal"
 # APPSECRET = "bb40d1721af5844611f285281584c1c1"
 
 # rtl f
-APPID = "wx832fa4d94a890b92"
-APPSECRET = "83b3a0283f6078179ec02bcac0608ca1"
+# APPID = "wx832fa4d94a890b92"
+# APPSECRET = "83b3a0283f6078179ec02bcac0608ca1"
