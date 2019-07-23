ecb67eea3dd5340967bc378e01fc77efd1b203b0
ljf <1062134582@qq.com>
2019-07-23 20:21:58 +0800

    日志信息输出到文件

--- common/log_print.py
+++ common/log_print.py
@@ -1,4 +1,11 @@
 import logging
+import os
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+log_path = os.path.join(os.path.dirname(os.path.dirname(__file__)) + '/log')
+
+if not os.path.exists(log_path):
+    os.mkdir(log_path)
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    filename=log_path + '/journal.log', filemode='a')
 logger = logging.getLogger(__name__)
